36533712217fece12bf11874d8ff3e59240226ac
abiSrtr <abi1997sag@gmail.com>
2017-07-08 14:21:34 +0530

    web page

---
+++ a.js
@@ -0,0 +1,27 @@
+
+
+
+var name = prompt("name");
+alert(name);
+
+var eda=document.getElementById("ed");
+var tda=eda.querySelectorAll("td");
+var ip=document.getElementById("hd");
+
+  for (var i = 0; i < tda.length; i++) {
+    tda[i].addEventListener("click",cl);
+    ip.addEventListener("blur",d);
+  }
+
+
+  function cl() {
+
+    this.className="sh";
+
+  }
+
+function d() {
+  var v=this.value;
+  console.log(v);
+  this.parentNode.innerHTML=v+hd;
+}
